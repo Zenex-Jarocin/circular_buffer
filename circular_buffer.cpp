149032a57b3f4e3ca76568064c831ef819e56748
emka <maciej.qb@gmail.com>
2021-12-09 21:26:29 +0100

    initial commit

---
+++ circular_buffer.cpp
@@ -0,0 +1,81 @@
+/*
+ * circular_buffer.cpp
+ *
+ *  Created on: Dec 6, 2021
+ *      Author: emka
+ */
+
+#include "circular_buffer.h"
+
+CircularBuffer::CircularBuffer()
+:
+m_bufferSize(DEFAULT_BUFFER_SIZE),
+m_topPointer(DEFAULT_BUFFER_SIZE - 1)
+{
+}
+
+CircularBuffer::CircularBuffer(uint8_t size)
+:
+m_bufferSize(size),
+m_topPointer(size - 1)
+{
+}
+
+cirBufErr_t CircularBuffer::put(const uint8_t item)
+{
+	if ((m_bufferSize - m_dataCounter) == 0)
+		return cirBufErr_t::ERR_FULL;
+
+	m_dataBuffer[m_putPointer] = item;
+	incrementPutPointer();
+	m_dataCounter++;
+	return cirBufErr_t::OK;
+
+}
+cirBufErr_t CircularBuffer::take(uint8_t* const pItem)
+{
+	if (m_dataCounter == 0)
+		return cirBufErr_t::ERR_EMPTY;
+
+	*pItem = m_dataBuffer[m_takePointer];
+	m_dataCounter--;
+	incrementTakePointer();
+	return cirBufErr_t::OK;
+}
+
+
+uint8_t CircularBuffer::getFreeSpace(void)
+{
+	if (m_putPointer == m_takePointer)
+		return m_dataCounter;
+
+	if (m_putPointer > m_takePointer)
+		return m_bufferSize - (m_putPointer - m_takePointer);
+
+	if (m_putPointer < m_takePointer)
+		return m_takePointer - m_putPointer;
+
+	return 0;
+}
+
+void CircularBuffer::incrementPutPointer(void)
+{
+	if (m_putPointer == m_topPointer)
+	{
+		m_putPointer = 0;
+		return;
+	}
+
+	m_putPointer++;
+}
+
+void CircularBuffer::incrementTakePointer(void)
+{
+	if (m_takePointer== m_topPointer)
+	{
+		m_takePointer = 0;
+		return;
+	}
+
+	m_takePointer++;
+}
---
+++ circular_buffer.h
@@ -0,0 +1,46 @@
+/*
+ * circular_buffer.h
+ *
+ *  Created on: Dec 6, 2021
+ *      Author: emka
+ */
+
+#ifndef CIRCULAR_BUFFER_H_
+#define CIRCULAR_BUFFER_H_
+
+#include <stdint.h>
+
+enum class cirBufErr_t
+{
+	OK,
+	ERR_FULL,
+	ERR_EMPTY,
+	OVERRIDE
+};
+
+constexpr uint8_t DEFAULT_BUFFER_SIZE = 64;
+class CircularBuffer
+{
+public:
+	CircularBuffer(void);
+	CircularBuffer(uint8_t size);
+	~CircularBuffer() = default;
+
+	cirBufErr_t put(const uint8_t item);
+	cirBufErr_t take(uint8_t* const pItem);
+
+private:
+	uint8_t m_dataBuffer[DEFAULT_BUFFER_SIZE];
+	uint8_t m_putPointer = 0;
+	uint8_t m_takePointer = 0;
+	const uint8_t m_bufferSize = 0;
+	const uint8_t m_topPointer = 0;
+	uint8_t m_dataCounter = 0;
+	uint8_t getFreeSpace(void);
+	void incrementPutPointer(void);
+	void incrementTakePointer(void);
+};
+
+
+
+#endif /* CIRCULAR_BUFFER_H_ */
